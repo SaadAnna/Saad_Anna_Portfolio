d006c14f35197eef3fd1ec5644e9bc763513699c
Saad Anna <saadannadev@gmail.com>
2025-03-03 14:20:11 +0100

    Start The Hero Section

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <section>
+      <div className="container">
+        <div>Hero</div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
