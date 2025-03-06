b466ebdedfbcb853cf58d5b3816ca9d32763762d
Saad Anna <saadannadev@gmail.com>
2025-03-06 17:47:38 +0100

    Start and finish the about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Navbar }  from "@/sections/Navbar";
 import { Hero } from "@/sections/Hero";
 import { LogoMotion } from "@/sections/LogoMotion";
 import { Services } from "@/sections/Services";
+import { About } from "@/sections/About";
 export default function Home() {
   return (
     <>
@@ -9,6 +10,7 @@ export default function Home() {
     <Hero />
     <LogoMotion />
     <Services />
+    <About />
     </>
       
   );
