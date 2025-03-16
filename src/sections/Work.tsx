399def444324acdfefe7093c4a343d40e9d09708
Saad Anna <saadannadev@gmail.com>
2025-03-16 03:44:25 +0100

    Update some design for the work section

--- src/sections/Work.tsx
+++ src/sections/Work.tsx
@@ -8,7 +8,8 @@ export const Work = () => {
       GitHubLink: false,
       description:
         "The AI SEO Generator Landing Page showcases an AI tool for creating optimized SEO content. It highlights features, benefits, and includes a call-to-action for user engagement.",
-    },
+      LivePreview: "https://dester.vercel.app/",
+      },
     {
       id: 2,
       type: "2024 / Side Project",
@@ -16,11 +17,9 @@ export const Work = () => {
       title: "Codew Startup Wesite",
       description:
         "A sleek, responsive landing page built with React.js, Vite, Tailwind CSS, and JavaScript. It highlights services, expertise, and includes a call-to-action to attract clients.",
-    },
-    {
-      id: 2,
-      GitHubLink: true,
-    },
+      LivePreview: "https://codewx.netlify.app/",
+      },
+   
   ];
   return (
     <section className="py-20">
@@ -29,12 +28,15 @@ export const Work = () => {
           <div className="text-5xl md:text-[70px] md:leading-none font-semibold tracking-tight bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
             My Success Stories
           </div>
-          <div className="flex p-2.5 py-16  rounded-xl gap-14 items-center lg:flex-1 flex-wrap justify-center">
+          <div className="flex p-2.5 py-16  rounded-xl gap-14 items-center lg:flex-1 flex-wrap justify-start">
             {Works.map((work) => (
+              
               <div
                 key={work.id}
+    
                 className="flex flex-col relative duration-200 ease-out hover:bg-neutral-950 text-start justify-start gap-2 p-2.5 border border-white/20 rounded-xl w-[300px] h-[300px] cursor-pointer"
               >
+                 <a href={work.LivePreview}>
                 <div className="text-sm text-white/70 font-medium">
                   {work.type}
                 </div>
@@ -44,6 +46,7 @@ export const Work = () => {
                 <div className=" text-white/70 tracking-tighter font-normal text-base max-w-3xl">
                   {work.description}
                 </div>
+               
                 {work.GitHubLink && (
                   <a
                     href="https://github.com/SaadAnna"
@@ -52,7 +55,8 @@ export const Work = () => {
                   >
                     View In GitHub
                   </a>
-                )}
+                  
+                )}</a>
               </div>
             ))}
           </div>
