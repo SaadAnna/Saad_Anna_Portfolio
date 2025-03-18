5443d0a8cdfdab1080c9466c1a01e4a7ee5663a9
Saad Anna <saadannadev@gmail.com>
2025-03-18 15:42:38 +0100

    Optomize The SEO Of The Project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google';
+import type { Metadata } from "next";
 import GridSquares from '@/Components/GridSquares';
 import { ReactNode } from 'react';
 import "./globals.css";
@@ -7,6 +8,33 @@ const inter = Inter({ subsets: ['latin'] });
 interface RootLayoutProps {
   children: ReactNode;
 }
+export const metadata: Metadata = {
+
+  title: "Saad Anna - Front-End Developer",
+  description: "Hello, I'm Saad Anna A Front-End Developer Wiht +2 Years Of Experience",
+  keywords: ["Developer", "Developers", "front-end", "portfolio", "morocco", "Saad", "HTML", "CSS", "javascript", "React.Js", "Next.Js", "TailwindCSS"],
+  openGraph: {
+    title: "Saad Anna - Front-End Developer",
+    description: "Hello, I'm Saad Anna A Front-End Developer Wiht +2 Years Of Experience",
+    images: [
+      {
+        url: "@/assets/logo.svg", // Replace with your OpenGraph image
+        width: 1200,
+        height: 630,
+        alt: "Saad Anna Portfolio Image",
+      },
+    ],
+    url: "https://dester.vercel.app/", // Replace with your website URL
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Saad Anna Portfolio",
+    description: "Front-End Developer",
+    images: ["@/assets/logo.svg"], // Replace with your Twitter image
+  },
+};
+
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
