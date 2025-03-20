f58751eb902ea0ba868bbcfe1954bd672bf84058
Saad Anna <saadannadev@gmail.com>
2025-03-20 12:26:43 +0100

    Change the size of the logo in the LogoMotion Section

--- src/sections/LogoMotion.tsx
+++ src/sections/LogoMotion.tsx
@@ -46,7 +46,7 @@ export const LogoMotion = () => {
                 vscodelogo,
               ].map((logo) => (
                 // eslint-disable-next-line @next/next/no-img-element
-                <img src={logo.src} key={logo.src} className="h-20 md:h-14 sm:h-6 w-auto" />
+                <img src={logo.src} key={logo.src} className="h-32 md:h-24 sm:h-6 w-auto" />
               ))}
             </motion.div>
           </div>
