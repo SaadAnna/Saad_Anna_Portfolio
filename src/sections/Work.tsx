93ecd4f5c3fe1a41e7cd4041dc3738d0a71586ee
Saad Anna <saadannadev@gmail.com>
2025-04-26 15:23:00 +0200

    Add some style in the work section

--- src/sections/Work.tsx
+++ src/sections/Work.tsx
@@ -34,13 +34,13 @@ export const Work = () => {
           <div className="text-5xl md:text-[70px] md:leading-none font-semibold tracking-tight bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
             My Success Stories
           </div>
-          <div className="flex p-2.5 py-16  rounded-xl gap-14 items-center lg:flex-1 flex-wrap justify-start">
+          <div className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
             {Works.map((work) => (
               
               <div
                 key={work.id}
     
-                className="flex flex-col relative duration-200 ease-out hover:bg-neutral-950 text-start justify-start gap-2 p-2.5 border border-white/20 rounded-xl w-[300px] h-[300px] cursor-pointer"
+                className="flex flex-col relative duration-200 ease-out hover:bg-neutral-950 text-start justify-start gap-3.5 p-3.5 border border-white/20 rounded-xl cursor-pointer"
               >
                  <a href={work.LivePreview} target="_blank">
                 <div className="text-sm text-white/70 font-medium">
@@ -49,7 +49,7 @@ export const Work = () => {
                 <div className="text-white tracking-tighter font-semibold text-2xl">
                   {work.title}
                 </div>
-                <div className=" text-white/70 tracking-tighter font-normal text-base max-w-3xl">
+                <div className=" text-white/70 tracking-tighter font-medium text-lg">
                   {work.description}
                 </div>       
                 </a>
